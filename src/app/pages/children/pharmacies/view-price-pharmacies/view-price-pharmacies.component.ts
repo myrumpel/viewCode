cd6f0999cd99751f08cd2efe24a761ba97aa1a1b
CherrySoup <Necromund@mail.ru>
2024-04-19 19:29:42 +0300

    -td;

---
+++ src/app/pages/children/pharmacies/view-price-pharmacies/view-price-pharmacies.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { contentDataRu } from '../../../../data/pages/contentData';
+import {fadeAnimation} from "../../../../../../../sklitFinish/src/app/animations/fade.animation";
+
+@Component({
+  selector: 'view-price-pharmacies',
+  templateUrl: './view-price-pharmacies.component.html',
+  styleUrl: './view-price-pharmacies.component.scss',
+  animations: [fadeAnimation],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ViewPricePharmaciesComponent {
+  data = contentDataRu.pharmacies.viewPricePharmacies;
+}
